cbf7b7e845156602a272e94ce4b975604ec82e3c
XxDrancerxX <idmdiazmora98@gmail.com>
2025-07-24 19:57:34 +0000

    first step

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+#Super user:
+# superuser
+# super@user.com
+# 123456
+
 class Trade(models.Model):    
     def __str__(self):
-        return f"{self.user.name} - {self.symbol} = {self.side} - @ {self.price}"
+        return f"{self.user.username} - {self.symbol} = {self.side} - @ {self.price}"
     
     symbol = models.CharField(max_length=10)
     price = models.DecimalField(max_digits=10,decimal_places=2)
@@ -17,7 +22,7 @@ class Trade(models.Model):
     ("SELL", "Sell"),
     ] 
     side = models.CharField(max_length=4, choices=SIDE_CHOICES)
-    excnahge = models.CharField(max_length=20)
+    exchange = models.CharField(max_length=20)
     currency = models.CharField(max_length=20)
     notes = models.TextField(blank=True, null=True)
 
