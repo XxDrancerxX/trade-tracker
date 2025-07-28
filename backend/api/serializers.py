bafa79e90616220c42653a4bfc2fbd9d1d1609f3
XxDrancerxX <idmdiazmora98@gmail.com>
2025-07-28 21:15:16 +0000

    Checking endpoints

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -6,7 +6,17 @@ class SpotTradeSerializer(serializers.ModelSerializer):
         model = SpotTrade
         fields = "__all__"
 
+    def to_internal_value(self, data):    
+        if "side" in data: 
+         data["side"] = data["side"].upper() ##We convert the data in uppercase since we have it as Capital in Models.py
+        return super().to_internal_value(data) ### Passes the modified data (with 'side' uppercased) to DRF’s base serializer logic to handle field validation and conversion.
+
 class FuturesTradeSerializer(serializers.ModelSerializer):
     class Meta:
         model = FuturesTrade
         fields = "__all__"        
+
+    def to_internal_value(self, data):    
+        if "side" in data:
+         data["side"] = data["side"].upper()
+        return super().to_internal_value(data)  
